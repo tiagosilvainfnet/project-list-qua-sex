df8ed0aab3b89916c56b1b95bd496bea7b1e5b0a
Tiago Silva <tiagoluizribeirodasilva@gmail.com>
2024-07-26 21:15:43 -0300

    - First route structure

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -23,15 +23,24 @@ export default function TabLayout() {
           ),
         }}
       />
-      <Tabs.Screen
-        name="explore"
+    <Tabs.Screen
+        name="dashboard"
         options={{
-          title: 'Explore',
-          tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon name={focused ? 'code-slash' : 'code-slash-outline'} color={color} />
-          ),
+            title: 'Dashboard',
+            tabBarIcon: ({ color, focused }) => (
+                <TabBarIcon name={focused ? 'code-slash' : 'code-slash-outline'} color={color} />
+            ),
         }}
-      />
+    />
+    <Tabs.Screen
+        name="profile"
+        options={{
+            title: 'Profile',
+            tabBarIcon: ({ color, focused }) => (
+                <TabBarIcon name={focused ? 'code-slash' : 'code-slash-outline'} color={color} />
+            ),
+        }}
+    />
     </Tabs>
   );
 }
