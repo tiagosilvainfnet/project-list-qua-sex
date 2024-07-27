56effc2501a61e432b52380f3b6901fc64119906
Tiago Silva <tiagoluizribeirodasilva@gmail.com>
2024-07-26 21:38:09 -0300

    - Finish route structure

--- .expo/types/router.d.ts
+++ .expo/types/router.d.ts
@@ -3,7 +3,7 @@ import type { ReactNode } from 'react';
 import type { TextProps, GestureResponderEvent } from 'react-native';
 
 export namespace ExpoRouter {
-  type StaticRoutes = `/` | `/(tabs)` | `/_sitemap` | `/explore`;
+  type StaticRoutes = `/` | `/(tabs)` | `/_sitemap` | `/dashboard` | `/form` | `/login` | `/profile` | `/register` | `/settings`;
   type DynamicRoutes<T extends string> = never;
   type DynamicRouteTemplate = never;
 
--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -14,33 +14,33 @@ export default function TabLayout() {
         tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
         headerShown: false,
       }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Home',
-          tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon name={focused ? 'home' : 'home-outline'} color={color} />
-          ),
-        }}
-      />
-    <Tabs.Screen
-        name="dashboard"
-        options={{
-            title: 'Dashboard',
-            tabBarIcon: ({ color, focused }) => (
-                <TabBarIcon name={focused ? 'code-slash' : 'code-slash-outline'} color={color} />
-            ),
-        }}
-    />
-    <Tabs.Screen
-        name="profile"
-        options={{
-            title: 'Profile',
-            tabBarIcon: ({ color, focused }) => (
-                <TabBarIcon name={focused ? 'code-slash' : 'code-slash-outline'} color={color} />
-            ),
-        }}
-    />
+        <Tabs.Screen
+            name="index"
+            options={{
+              title: 'Home',
+              tabBarIcon: ({ color, focused }) => (
+                <TabBarIcon name={focused ? 'home' : 'home-outline'} color={color} />
+              ),
+            }}
+        />
+        <Tabs.Screen
+            name="dashboard"
+            options={{
+                title: 'Dashboard',
+                tabBarIcon: ({ color, focused }) => (
+                    <TabBarIcon name={focused ? 'analytics' : 'analytics-outline'} color={color} />
+                ),
+            }}
+        />
+        <Tabs.Screen
+            name="profile"
+            options={{
+                title: 'Profile',
+                tabBarIcon: ({ color, focused }) => (
+                    <TabBarIcon name={focused ? 'people' : 'people-outline'} color={color} />
+                ),
+            }}
+        />
     </Tabs>
   );
 }
