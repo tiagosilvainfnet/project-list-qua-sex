3887702ba65d59ce99fbf60a4a58459dedfe5c76
Tiago Silva <tiagoluizribeirodasilva@gmail.com>
2024-08-16 22:03:17 -0300

    Login Screen

--- app/login.tsx
+++ app/login.tsx
@@ -1,24 +1,82 @@
-import {Text, View} from 'react-native';
-import {Button, Grid, TextInput} from "@/components";
+import {ScrollView} from 'react-native';
+import {Avatar, Button, Grid, TextInput} from "@/components";
 import {useSession} from "@/app/ctx";
-import {router} from "expo-router";
+import {Link} from "expo-router";
+import {useState} from "react";
 
 export default function LoginScreen() {
     const { signIn } = useSession();
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
 
     return (
-        <View style={{
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center',
-            height: '100%'
-        }}>
-            <Grid>
-                <TextInput />
+        <ScrollView>
+            <Grid style={{
+                display: 'flex',
+                justifyContent: 'center',
+                height: '100%'
+            }}>
+                <Grid style={{
+                    marginTop: 50,
+                    ...styles.container,
+                    ...styles.padding
+                }}>
+                    <Avatar size={200} source={require('../assets/images/logo.jpg')}/>
+                </Grid>
+                <Grid style={{
+                    ...styles.padding
+                }}>
+                    <TextInput
+                        value={email}
+                        keyboardType="email-address"
+                        onChangeText={setEmail}
+                        label="E-mail"
+                    />
+                </Grid>
+                <Grid style={{
+                    ...styles.padding
+                }}>
+                    <TextInput
+                        value={password}
+                        onChangeText={setPassword}
+                        label="Senha"
+                        secureTextEntry={true}
+                    />
+                </Grid>
+                <Grid style={{
+                    ...styles.padding,
+                    ...styles.container,
+                    textAlign: 'center'
+                }}>
+                    {/*@ts-ignore*/}
+                    <Link href="register">
+                        Criar conta
+                    </Link>
+                </Grid>
+                <Grid style={{
+                    ...styles.padding
+                }}>
+                    <Button
+                        style={{
+                            borderRadius: 0
+                        }}
+                        mode="contained"
+                        onPress={signIn}>
+                        Entrar
+                    </Button>
+                </Grid>
             </Grid>
-            <Button onPress={signIn}>
-                Entrar
-            </Button>
-        </View>
+        </ScrollView>
     );
+}
+
+const styles = {
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    padding: {
+        padding: 16,
+    }
 }
