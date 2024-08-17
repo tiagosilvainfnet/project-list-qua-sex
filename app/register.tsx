20b94516e3c7b6be55c0c84e1b2e27ba5c2dd864
Tiago Silva <tiagoluizribeirodasilva@gmail.com>
2024-08-16 22:09:30 -0300

    Registro (#33)

--- app/register.tsx
+++ app/register.tsx
@@ -1,9 +1,92 @@
-import { Text } from 'react-native';
+import {ScrollView} from 'react-native';
+import {Avatar, Button, Grid, TextInput} from "@/components";
+import {useSession} from "@/app/ctx";
+import {Link} from "expo-router";
+import {useState} from "react";
 
 export default function RegisterScreen() {
+    const { signUp } = useSession();
+    const [email, setEmail] = useState('');
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
     return (
-        <Text>
-            Register
-        </Text>
+        <ScrollView>
+            <Grid style={{
+                display: 'flex',
+                justifyContent: 'center',
+                height: '100%'
+            }}>
+                <Grid style={{
+                    marginTop: 50,
+                    ...styles.container,
+                    ...styles.padding
+                }}>
+                    <Avatar size={200} source={require('../assets/images/logo.jpg')}/>
+                </Grid>
+                <Grid style={{
+                    ...styles.padding
+                }}>
+                    <TextInput
+                        value={email}
+                        keyboardType="email-address"
+                        onChangeText={setEmail}
+                        label="E-mail"
+                    />
+                </Grid>
+                <Grid style={{
+                    ...styles.padding
+                }}>
+                    <TextInput
+                        value={username}
+                        onChangeText={setUsername}
+                        label="Nome de Usuário"
+                    />
+                </Grid>
+                <Grid style={{
+                    ...styles.padding
+                }}>
+                    <TextInput
+                        value={password}
+                        onChangeText={setPassword}
+                        label="Senha"
+                        secureTextEntry={true}
+                    />
+                </Grid>
+                <Grid style={{
+                    ...styles.padding,
+                    ...styles.container,
+                    textAlign: 'center'
+                }}>
+                    {/*@ts-ignore*/}
+                    <Link href="login">
+                        Fazer login
+                    </Link>
+                </Grid>
+                <Grid style={{
+                    ...styles.padding
+                }}>
+                    <Button
+                        style={{
+                            borderRadius: 0
+                        }}
+                        mode="contained"
+                        onPress={signUp}>
+                        Cadastrar
+                    </Button>
+                </Grid>
+            </Grid>
+        </ScrollView>
     );
+}
+
+const styles = {
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    padding: {
+        padding: 16,
+    }
 }
