08edea780f9ca5fd71f9d42ba5c226fd4c2dcfdc
Tiago Silva <tiagoluizribeirodasilva@gmail.com>
2024-08-21 21:36:47 -0300

    Esqueci minha senha

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,14 +1,21 @@
 import {Navigator} from "expo-router";
 import Slot = Navigator.Slot;
-import {SessionProvider} from "@/app/ctx";
+import {SessionProvider, useSession} from "@/app/ctx";
 import {PaperProvider} from "react-native-paper";
 import {useColorScheme} from "react-native";
 import {darkTheme, lightTheme} from "@/constants/Theme";
+import {useStorageState} from "@/app/useStorageState";
 
 export default function RootLayout() {
     const themeType = useColorScheme();
+    const [[isLoadingTheme, theme], setTheme] = useStorageState('theme');
+    const themeJson = {
+        'dark': darkTheme,
+        'light': lightTheme
+    }
 
-    return  <PaperProvider theme={themeType === "dark" ? darkTheme : lightTheme}>
+    // @ts-ignore
+    return  <PaperProvider theme={theme === "auto" || theme === null ? themeType === "dark" ? themeJson['dark'] : themeJson['light'] :  themeJson[theme]}>
                 <SessionProvider>
                     <Slot />
                 </SessionProvider>
