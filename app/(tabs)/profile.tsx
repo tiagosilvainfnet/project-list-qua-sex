a40391cbfe163922010f558974199b205d1aa250
Tiago Silva <tiagoluizribeirodasilva@gmail.com>
2024-08-23 20:55:19 -0300

    Tela de perfil (#37)

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,7 +1,100 @@
-import {Topbar} from "@/components";
+import {Avatar, Button, Fab, Grid, TextInput, Topbar} from "@/components";
+import {useState} from "react";
 
 export default function ProfileScreen() {
-    return (
-        <Topbar title="Perfil"/>
-    );
+    const [image, setImage] = useState(null);
+
+    return  <Grid>
+                <Grid>
+                    <Topbar title="Perfil"/>
+                </Grid>
+                <Grid>
+                    <Grid style={{
+                        ...styles.containerImage
+                    }}>
+                        <Grid style={{
+                            ...styles.containerCenterImage
+                        }}>
+                            {
+                                image ? <Avatar size={230} source={require('../../assets/images/logo.jpg')} /> : <Avatar size={230} icon="account" />
+                            }
+                            <Fab
+                                icon="image"
+                                style={{
+                                    ...styles.fab,
+                                    ...styles.left
+                                }}/>
+                            <Fab
+                                icon="camera"
+                                style={{
+                                    ...styles.fab,
+                                    ...styles.right
+                                }}/>
+                        </Grid>
+                    </Grid>
+                </Grid>
+                <Grid style={{
+                    marginTop: 30,
+                    ...styles.padding
+                }}>
+                    <TextInput
+                        label="Nome"
+                    />
+                </Grid>
+                <Grid style={{
+                    ...styles.padding
+                }}>
+                    <TextInput
+                        label="Sobrenome"
+                    />
+                </Grid>
+                <Grid style={{
+                    ...styles.padding
+                }}>
+                    <TextInput
+                        label="Nome de usuário"
+                    />
+                </Grid>
+                <Grid style={{
+                    ...styles.padding
+                }}>
+                    <TextInput
+                        label="E-mail"
+                        keyboardType="email-address"
+                    />
+                </Grid>
+                <Grid style={{
+                    ...styles.padding
+                }}>
+                    <Button mode="contained" onPress={() => {}}>
+                        Salvar
+                    </Button>
+                </Grid>
+            </Grid>
+}
+
+const styles = {
+    containerImage: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginTop: 20,
+    },
+    padding: {
+        padding: 16
+    },
+    containerCenterImage: {
+        width: 230,
+        position: 'relative',
+    },
+    fab: {
+        bottom: 0,
+        position: 'absolute',
+        borderRadius: 200
+    },
+    right: {
+        right: 0,
+    },
+    left: {
+        left: 0
+    }
 }
--- app/form.tsx
+++ app/form.tsx
@@ -1,9 +1,15 @@
 import { Text } from 'react-native';
+import {Fab, Grid, Topbar} from "@/components";
+import {router} from "expo-router";
 
 export default function FormScreen() {
-    return (
-        <Text>
-            Form
-        </Text>
-    );
+    return <Grid style={{
+                height: '100%',
+                width: '100%',
+            }}>
+                <Grid>
+                    <Topbar title="Novo item" back={true} menu={false}/>
+                </Grid>
+                <Grid></Grid>
+            </Grid>;
 }
